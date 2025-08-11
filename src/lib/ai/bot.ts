6d9b43f54d7508527904fd5617320bde25a72a95
Swagatam <swagatamheroz@outlook.com>
2025-08-11 22:44:32 +0530

    chore: reconfigure function for context passing

--- src/lib/ai/bot.ts
+++ src/lib/ai/bot.ts
@@ -1,15 +1,18 @@
 import { createOpenRouter } from "@openrouter/ai-sdk-provider";
 import { streamText } from "ai";
 
-export async function bot(modelName: string, prompt: string) {
+export async function bot(
+  modelName: string,
+  messages: { role: "system" | "user" | "assistant"; content: string }[]
+) {
   const openRouter = createOpenRouter({
     apiKey: process.env.OPENROUTER_API_KEY,
   });
 
   const { textStream } = streamText({
     model: openRouter(modelName),
-    prompt,
     temperature: 0.5,
+    messages,
   });
 
   return textStream;
